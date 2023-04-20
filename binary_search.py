7df9d0708a518c7ede203f3e89bbf3ac1c130e2f
Krzysztof Gruca <kgruca21@gmail.com>
2023-04-19 23:29:02 -0700

    practicing binary search

---
+++ binary_search.py
@@ -0,0 +1,22 @@
+def binary_search(list, value):
+    low = 0
+    high = len(list) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+        attempt = list[mid]
+        if attempt == value:
+            return mid
+        elif attempt > value:
+            high = mid - 1
+        else:
+            low = mid + 1
+
+    return None
+
+
+new_list = [1, 4, 6, 7, 12, 23, 56, 89, 123, 232, 491, 519, 594, 607, 1235]
+
+print(binary_search(new_list, 607))
+print(binary_search(new_list, 23))
+print(binary_search(new_list, 2))
