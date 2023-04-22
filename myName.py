09745f237ac710bc8c1803c63d1787bd8d824baa
Krzysztof Gruca <kgruca21@gmail.com>
2023-04-21 19:03:07 -0700

    some more binary search practice

--- myName.py
+++
@@ -1,9 +0,0 @@
-
-print('Hello world!')
-print('What is your name')
-myName = input()
-print("It's good to meet you,{myName}".format(myName=myName))
-print("The length of your name is {len}".format(len=len(myName)))
-print('What is your age?')
-myAge = input()
-print("You will be " + str(int(myAge) + 1) + " in a year")
