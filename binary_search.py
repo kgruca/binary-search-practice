be30119fd7421bc761f7f0c05b3f97fa05618b9b
Krzysztof Gruca <kgruca21@gmail.com>
2023-04-23 23:22:17 -0700

    some more bin search practice

--- binary_search.py
+++ binary_search.py
@@ -1,13 +1,37 @@
-def binary_search(list, value):
+# def binary_search(list, value):
+#     low = 0
+#     high = len(list) - 1
+
+#     while low <= high:
+#         mid = (low + high) // 2
+#         attempt = list[mid]
+#         if attempt == value:
+#             return mid
+#         elif attempt > value:
+#             high = mid - 1
+#         else:
+#             low = mid + 1
+
+#     return None
+
+
+# new_list = [1, 4, 6, 7, 12, 23, 56, 89, 123, 232, 491, 519, 594, 607, 1235]
+
+# print(binary_search(new_list, 607))
+# print(binary_search(new_list, 23))
+# print(binary_search(new_list, 2))
+
+
+def binary_search_practice(arr, val):
     low = 0
-    high = len(list) - 1
+    high = len(arr) - 1
 
     while low <= high:
         mid = (low + high) // 2
-        attempt = list[mid]
-        if attempt == value:
+
+        if val == arr[mid]:
             return mid
-        elif attempt > value:
+        elif val < arr[mid]:
             high = mid - 1
         else:
             low = mid + 1
@@ -15,8 +39,14 @@ def binary_search(list, value):
     return None
 
 
-new_list = [1, 4, 6, 7, 12, 23, 56, 89, 123, 232, 491, 519, 594, 607, 1235]
+new_arr = [1, 3, 5, 7, 8, 11, 14, 19]
 
-print(binary_search(new_list, 607))
-print(binary_search(new_list, 23))
-print(binary_search(new_list, 2))
+print(binary_search_practice(new_arr, 1))
+print(binary_search_practice(new_arr, 3))
+print(binary_search_practice(new_arr, 4))
+print(binary_search_practice(new_arr, 5))
+print(binary_search_practice(new_arr, 7))
+print(binary_search_practice(new_arr, 8))
+print(binary_search_practice(new_arr, 11))
+print(binary_search_practice(new_arr, 14))
+print(binary_search_practice(new_arr, 19))
