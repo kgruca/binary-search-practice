a3eeeb256c96ec665fac81aef9c89b8b4ab4412f
Krzysztof Gruca <kgruca21@gmail.com>
2023-04-28 19:29:58 -0700

    binary search practice 4.28.23

--- bin_search.py
+++ bin_search.py
@@ -1,14 +1,37 @@
-def binary_search(list, value):
+# def binary_search(list, value):
+#     low = 0
+#     high = len(list) - 1
+
+#     while low <= high:
+#         mid = (low + high) // 2
+#         attempt = list[mid]
+
+#         if attempt == value:
+#             return mid
+#         elif attempt > value:
+#             high = mid - 1
+#         else:
+#             low = mid + 1
+
+#     return None
+
+
+# new_list = ["as", "fgd", "gti", "h", "itwe", "klw"]
+
+# print(binary_search(new_list, "fgd"))
+
+
+def binary_search(arr, val):
     low = 0
-    high = len(list) - 1
+    high = len(arr) - 1
 
     while low <= high:
-        mid = (low + high) // 2
-        attempt = list[mid]
+        mid = low + (high - low) // 2
+        mid_val = arr[mid]
 
-        if attempt == value:
+        if mid_val == val:
             return mid
-        elif attempt > value:
+        elif mid_val > val:
             high = mid - 1
         else:
             low = mid + 1
@@ -16,6 +39,6 @@ def binary_search(list, value):
     return None
 
 
-new_list = ["as", "fgd", "gti", "h", "itwe", "klw"]
+new_arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-print(binary_search(new_list, "fgd"))
+print(binary_search(new_arr, 1))
